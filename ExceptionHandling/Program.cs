243590396521ed582e9d729b8afde0c0fc007ca2
Allen <allenlozada24@gmail.com>
2025-09-13 15:59:00 +0800

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler(exceptionHandlerApp =>
+    {
+        exceptionHandlerApp.Run(async context =>
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            // will be overrriden bc of writeasjsonasync method
+            //context.Response.ContentType = Text.Plain;
+            
+
+            var exceptionHandlerPathFeature =
+                context.Features.Get<IExceptionHandlerPathFeature>();
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = exceptionHandlerPathFeature?.Error.Message,
+                path = exceptionHandlerPathFeature?.Path
+            });
+
+
+            // write as plain text
+            //await context.Response.WriteAsync(
+            //    $"Error: {exceptionHandlerPathFeature?.Error.Message}\n" +
+            //    $"Path: {exceptionHandlerPathFeature?.Path}"
+            //);
+        });
+    });
+}
+
+app.UseStatusCodePages(async statusCodeContext =>
+{
+    // using static System.Net.Mime.MediaTypeNames;
+    statusCodeContext.HttpContext.Response.ContentType = Text.Plain;
+
+    await statusCodeContext.HttpContext.Response.WriteAsync(
+        $"Status Code Page: {statusCodeContext.HttpContext.Response.StatusCode}");
+});
+
+//redirects to an url
+app.UseStatusCodePagesWithRedirects("/Error/{0}");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
